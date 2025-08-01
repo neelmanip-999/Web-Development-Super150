0a1bcb80d0aa4d1639c4c413dfde35a77439a32b
neelmanip-999 <neelmani.p999@gmail.com>
2025-08-01 13:23:39 +0530

    Ternary operator

--- React.JS/Lecture_1/first-app/src/Component/Sam.js
+++ React.JS/Lecture_1/first-app/src/Component/Sam.js
@@ -1,20 +1,22 @@
 import React from 'react';
 
 let arr2 = ["😊", "❤️", "🔥", "🙌", "🎉", "⭐"];
-
 let todos = ["day1", "day2", "day3", "day4", "day5", "day6"];
-
 let emojs = Math.floor(Math.random() * 6);
 
 const Sam = () => {
   return (
     <div>
-      <h3>arr2[emojs]</h3>
-      {todos.map((item, index) => (
-        <li key={index}>
-          my task is {item} &amp; position is {index}
-        </li>
-      ))}
+      <h3>{arr2[emojs]}</h3>
+      <ul>
+        {todos.map((item, index) => (
+          <li key={index}>
+            {index % 2 === 0
+              ? `⭐ Important task: ${item} at position ${index}`
+              : `Regular task: ${item} at position ${index}`}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
